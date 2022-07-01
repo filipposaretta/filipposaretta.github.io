f0f49e23b3d819dced41474816fa790412828719
filipposaretta <filipposaretta@gmail.com>
2022-07-01 14:12:00 +0200

    First commit

---
+++ js/counter.js
@@ -0,0 +1,11 @@
+let num = 0;
+
+function aggiungi() {
+    num ++;
+    document.getElementById("num").innerHTML = num;
+}
+
+function sottrai() {
+    num--;
+    document.getElementById("num").innerHTML = num;
+}
