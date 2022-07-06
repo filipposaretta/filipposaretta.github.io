d1532114345182888012058fab86c9889b4d232d
filipposaretta <filipposaretta@gmail.com>
2022-07-06 21:36:02 +0200

    Text and buttons added in JS

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,17 @@
 let num = 0;
 
+let counter = document.createElement("h2");
+counter.id = "num";
+counter.innerHTML = num;
+
+window.onload = function() {
+    document.getElementById("add").addEventListener("click", aggiungi);
+    document.getElementById("subtract").addEventListener("click", sottrai);
+    document.body.childNodes[1].after(counter);
+}
+
 function aggiungi() {
-    num ++;
+    num++;
     document.getElementById("num").innerHTML = num;
 }
 
